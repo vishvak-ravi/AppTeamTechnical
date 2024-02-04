e074fa6d740cb992e42b95aeb0350a81db69b84a
Vish Ravichandran <vishravi03@gmail.com>
2024-02-04 12:56:44 -0500

    Initial Commit

---
+++ AppTeamTechnical/AppTeamTechnicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTeamTechnicalApp.swift
+//  AppTeamTechnical
+//
+//  Created by Vishvak Ravi on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AppTeamTechnicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
